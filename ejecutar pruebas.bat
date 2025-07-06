b389eb585b546994a90f4e5325d0766bba9de2cf
Nacho Martínez <naching13@gmail.com>
2025-07-06 14:21:42 -0300

    Update ejecutar pruebas.bat

--- ejecutar pruebas.bat
+++ ejecutar pruebas.bat
@@ -3,5 +3,5 @@ title Ejecutor de Pruebas QA
 echo ==============================
 echo  Iniciando pruebas automatizadas
 echo ==============================
-python C:\Users\user\Desktop\qa_automation_project\run_tests.py
+python \qa_automation_project\run_tests.py
 pause
